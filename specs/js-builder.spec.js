7f184d87da29b1753290ee27d29ce2ab07ca3cde
malas34 <malas34.github@gmail.com>
2014-10-24 18:02:31 +0200

    Upd spec

--- specs/js-builder.spec.js
+++ specs/js-builder.spec.js
@@ -1,5 +1,5 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, browser: true, regexp: true */
-/*global require */
+/*global require, describe, beforeEach, it, xit */
 (function () {
 
     'use strict';
@@ -12,4 +12,4 @@
 
     });
 
-});
+}());
